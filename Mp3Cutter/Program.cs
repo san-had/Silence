0cceb6396cb0276303af69e483af84d47172fde8
san-had <hadhazis@gmail.com>
2017-12-26 18:20:00 +0100

    Added merge functionality

--- Mp3Cutter/Program.cs
+++ Mp3Cutter/Program.cs
@@ -6,26 +6,29 @@
 
     class Program
     {
-        private static int beginCut = 3090;  // seconds
-        private static int endCut = 3175;
-        private static string mp3Path = @"D:\mp3\20171022.mp3";
+        private static int beginCut = 409;  // seconds
+        private static int endCut = 506;
+        private static string mp3Path = @"D:\mp3\20171225a.mp3";
+        private static string mp3Path2 = @"D:\mp3\20171225b.mp3";
         private static double frameProSec = 19.14183087027915;
 
         static void Main(string[] args)
         {
-            int totalFrameCount = GetTotalFrameCount(mp3Path);
+            //int totalFrameCount = GetTotalFrameCount(mp3Path);
 
-            int totalTimeLength = GetTotalTimeLength(mp3Path);
+            //int totalTimeLength = GetTotalTimeLength(mp3Path);
 
-            double calculatedFrameProSec = GetFrameProSec(totalFrameCount, totalTimeLength);
+            //double calculatedFrameProSec = GetFrameProSec(totalFrameCount, totalTimeLength);
 
-            CuttingMp3(beginCut, endCut, mp3Path, calculatedFrameProSec);
+            //CuttingMp3(beginCut, endCut, mp3Path, calculatedFrameProSec);
 
-            Console.WriteLine($"Total frame count: {totalFrameCount}");
+            //Console.WriteLine($"Total frame count: {totalFrameCount}");
 
-            Console.WriteLine($"Total time length: {totalTimeLength}");
+            //Console.WriteLine($"Total time length: {totalTimeLength}");
 
-            Console.WriteLine($"FrameProSec: {calculatedFrameProSec}");
+            //Console.WriteLine($"FrameProSec: {calculatedFrameProSec}");
+
+            MergingMp3(mp3Path, mp3Path2);
 
             Console.Read();
         }
@@ -103,6 +106,47 @@
 
                 if (writer != null) writer.Dispose();
             }
+        }
+
+        private static void MergingMp3(string mp3Path, string mp3Path2)
+        {
+            var mp3Dir = Path.GetDirectoryName(mp3Path);
+            var mp3File = Path.GetFileName(mp3Path);
+            var splitDir = Path.Combine(mp3Dir, Path.GetFileNameWithoutExtension(mp3Path));
+            Directory.CreateDirectory(splitDir);
+
+            int splitI = 0;
+            FileStream writer = null;
+            Action createWriter = new Action(() => {
+                writer = File.Create(Path.Combine(splitDir, Path.ChangeExtension(mp3File, (++splitI).ToString("D4") + ".mp3")));
+            });
+
+            int totalFrameCount = 0;
+
+            using (var reader = new Mp3FileReader(mp3Path))
+            {
+                Mp3Frame frame;
+                while ((frame = reader.ReadNextFrame()) != null)
+                {
+                    if (writer == null) createWriter();
+
+                    writer.Write(frame.RawData, 0, frame.RawData.Length);
+                }
+            }
+
+            using (var reader = new Mp3FileReader(mp3Path2))
+            {
+                Mp3Frame frame;
+                while ((frame = reader.ReadNextFrame()) != null)
+                {
+                    if (writer == null) createWriter();
+
+                    writer.Write(frame.RawData, 0, frame.RawData.Length);
+                    ++totalFrameCount;
+                }
+            }
+
+            if (writer != null) writer.Dispose();
         }
     }
 }

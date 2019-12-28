41a7038d53c05861fb765537f5a3b97efb5cd7e3
san-had <hadhazis@gmail.com>
2019-12-28 11:29:19 +0100

    added unit tests

---
+++ Mp3Cutter.Tests.Unit/Mp3InputSetterTests.cs
@@ -0,0 +1,40 @@
+﻿using Mp3CutterExtensibility.Dto;
+using Mp3CutterService;
+using Xunit;
+
+namespace Mp3Cutter.Tests.Unit
+{
+    public class Mp3InputSetterTests
+    {
+        [Fact]
+        public void TimeCalculationTest()
+        {
+            // Arrange
+            CuttingTimeDto cuttingTimeDto = new CuttingTimeDto
+            {
+                BeginHour = 2,
+                BeginMinute = 18,
+                BeginSecond = 36,
+
+                EndHour = 3,
+                EndMinute = 32,
+                EndSecond = 54
+            };
+
+            string mp3FileName = @"C:\Hello.mp3";
+            int index = 1;
+
+            Mp3InputSetter mp3InputSetter = new Mp3InputSetter();
+
+            // Act
+            Mp3InputDto mp3InputDto = mp3InputSetter.SetMp3InputDto(cuttingTimeDto, mp3FileName, index);
+
+            // Assert
+            int expectedBeginCut = 8316;
+            int expectedEndCut = 12774;
+
+            Assert.Equal(expectedBeginCut, mp3InputDto.BeginCut);
+            Assert.Equal(expectedEndCut, mp3InputDto.EndCut);
+        }
+    }
+}
---
+++ Mp3Cutter.Tests.Unit/Mp3OutputSetterTests.cs
@@ -0,0 +1,28 @@
+﻿using Mp3CutterService;
+using Xunit;
+
+namespace Mp3Cutter.Tests.Unit
+{
+    public class Mp3OutputSetterTests
+    {
+        [Fact]
+        public void OutputSettingTest()
+        {
+            // Arrange
+            int index = 1;
+            string mp3Path = @"D:\mp3\20191208.mp3";
+
+            Mp3OutputSetter mp3OutputSetter = new Mp3OutputSetter();
+
+            // Act
+            var mp3OutputDto = mp3OutputSetter.SetMp3OutputDto(index, mp3Path);
+
+            // Assert
+            string expectedOutputDir = @"D:\mp3";
+            string expectedOutputFileName = @"D:\mp3\20191208.cut1.mp3";
+
+            Assert.Equal(expectedOutputDir, mp3OutputDto.OutputDir);
+            Assert.Equal(expectedOutputFileName, mp3OutputDto.Mp3OutputFileName);
+        }
+    }
+}
